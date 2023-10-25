bdcbea79079ca7fbdbbc1c164e068536ce006a2a
neerajAIML <146606686+neerajAIML@users.noreply.github.com>
2023-10-25 19:40:14 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+#Database settings
+# username = "postgres", password = '123456789', host = "127.0.0.1", port = "5432"
+PostgreSQL_Connection = 'postgresql://postgres:123456789@localhost/instagram'
+Sqlite_Connection = 'sqlite:///database.db'
+
+#Instagram Setting
+insta_name = ''
+insta_password = ''
+tables_name = 'instagram'
+#twitter setting
+
+#facebook setting
